7484bc12f63cf07d427b605b92161c93cbdff995
Martin Pek <Martin.Pek@web.de>
2022-05-30 12:22:16 +0200

    updated test.cpp with strtok multiple delimiter example

--- test.cpp
+++ test.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
-#include <cstring>
-
 using namespace std;
-// strcat
-char testString[32] = "";
+#include <stdio.h>
+#include <string.h>
+
+char rcvd[] = "!Relay_1_1\n!line_2_2\nline_3_3";
 
 int main() {
-    strcat(testString, "!Relay_3_3");
-    cout << testString << endl;
-    strcat(testString, "!Relay_6_9");
-    cout << testString << endl;
+
+  char delimiter[2] = "_";
+  int lineCnt = 0;
+  char* line = strtok(rcvd, "\n"); 
+  
+  while (line != NULL) {
+    lineCnt++;
+    line = strtok(NULL, "\n");
+  }
+
+  cout << "v12312" << endl;
+  cout << "lineCnt is: " << lineCnt << endl;
+  line = rcvd;
+  int lineLenght = 0;
+
+  for (int lineNo = 0; lineNo < lineCnt; lineNo++) {
+    lineLenght = strlen(line);
+    cout << "line is: " << line << endl;
+    cout << "lineNo is: " << lineNo << endl;
+    
+    char* splits = strtok(line, delimiter);
+    while (splits != NULL) {
+      cout << splits << endl;
+      splits = strtok(NULL, delimiter);
+    }
+    
+    line += lineLenght + 1;
+    cout << "endLoop" << endl;
+  }
+
+    /*
+    char* splits = strtok(line, delimiter);
+    cout << splits << endl;
+
+    line += strlen(line) + 1;
+    // line = strtok(line, "\n");
+    cout << line << endl;
+    break;
+    */
 }
