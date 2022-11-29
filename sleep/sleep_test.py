c8116dd0b59f280465b62ec5edc3a7bb99dce78f
Martin Pek <Martin.Pek@web.de>
2022-11-29 01:26:54 +0100

    working: sleep script, may be a bit agressive on low load inputs

--- sleep/sleep_test.py
+++ sleep/sleep_test.py
@@ -2,6 +2,7 @@ import psutil
 import time
 from time import sleep
 import json
+import os
 
 
 # or maybe https://docs.python.org/2/library/configparser.html
@@ -14,7 +15,6 @@ class Settings:
                 self.cpu_interval_threshold = cfg["cpu_interval_threshold"]
                 self.cpu_load_threshold = cfg["cpu_load_threshold"]
                 self.net_interval_threshold = cfg["net_interval_threshold"]
-                self.net_load_threshold = cfg["net_load_threshold"]
         except ValueError as e:
             print('failure to read config.json')
             print(e)
@@ -24,12 +24,11 @@ class Settings:
 settings = Settings()
 
 
-def net_usage(interval=1):  # change the inf variable according to the interface
+def get_net_usage(interval=1):  # change the inf variable according to the interface
     net_in, net_out = 0, 0
 
     ret = psutil.net_io_counters(pernic=True, nowrap=True)
     for key, value in ret.items():
-        print(value)
         net_in -= value.bytes_recv
         net_out -= value.bytes_sent
 
@@ -42,36 +41,40 @@ def net_usage(interval=1):  # change the inf variable according to the interface
     net_in = round(net_in / 1024 / 1024 / interval, 3)
     net_out = round(net_out / 1024 / 1024 / interval, 3)
 
-    print(f"Current net-usage:\nIN: {net_in} MB/s, OUT: {net_out} MB/s")
+    # print(f"Current net-usage:\nIN: {net_in} MB/s, OUT: {net_out} MB/s")
+    return net_in, net_out
 
 
 def main():
-    net_usage()
 
     low_cpu_intervals = low_net_intervals = 0
     cpu_load_threshold = 5
     cpu_interval_threshold = 10
-    net_interval_threshold = 3
     net_load_threshold = 3
+    # 0.61 MB/s
+    # 0.01
 
-    #  When interval is 0.0 or None compares system CPU times elapsed since last call or module import, returning immediately.
-    print(psutil.cpu_percent())
+    #  When interval is 0.0 or None compares system CPU times elapsed since last call or module import
+    psutil.cpu_percent()
 
-    # https://stackoverflow.com/questions/911856/detecting-idle-time-using-python
-
-    while low_cpu_intervals < cpu_interval_threshold and low_net_intervals < net_interval_threshold:
-        if psutil.cpu_percent() > cpu_load_threshold:
+    while True:
+        net_in, net_out = get_net_usage(1)
+        if net_in < 0.02 or net_out < 0.01:
+            low_net_intervals += 1
+        else:
+            low_net_intervals = 0
+        print(psutil.cpu_percent())
+        if psutil.cpu_percent() < cpu_load_threshold:
             low_cpu_intervals += 1
         else:
             low_cpu_intervals = 0
-        print(low_cpu_intervals)
+
+        if low_net_intervals > net_load_threshold and low_cpu_intervals > cpu_interval_threshold:
+            # maybe make a popup prompts here
+            os.system("shutdown /s /t 1")
+
+        print(f"low_cpu_intervals {low_cpu_intervals} \n low_network {low_net_intervals}")
         sleep(1)
-
-        # cpu_usage()
-        # net_usage()
-
-    print("shutdown")
-    # os.system("shutdown /s /t 1")
 
 
 if __name__ == '__main__':
