293fc526b2ef0eda5e8f2726ca6954458502a7ec
MartinPek <martin_pek@web.de>
2022-12-03 22:16:22 +0100

    sleep: moved some varaibles to config.json

--- sleep/sleep_test.py
+++ sleep/sleep_test.py
@@ -15,13 +15,16 @@ class Settings:
                 self.cpu_interval_threshold = cfg["cpu_interval_threshold"]
                 self.cpu_load_threshold = cfg["cpu_load_threshold"]
                 self.net_interval_threshold = cfg["net_interval_threshold"]
+                self.net_in_threshold = cfg["net_in_threshold"]
+                self.net_out_threshold = cfg["net_out_threshold"]
+                self.update_interval = cfg["update_interval"]
         except ValueError as e:
             print('failure to read config.json')
             print(e)
             exit()
 
 
-settings = Settings()
+setts = Settings()
 
 
 def get_net_usage(interval=1):  # change the inf variable according to the interface
@@ -48,33 +51,29 @@ def get_net_usage(interval=1):  # change the inf variable according to the inter
 def main():
 
     low_cpu_intervals = low_net_intervals = 0
-    cpu_load_threshold = 5
-    cpu_interval_threshold = 10
-    net_load_threshold = 3
     # 0.61 MB/s
     # 0.01
 
-    #  When interval is 0.0 or None compares system CPU times elapsed since last call or module import
+    # When interval is 0.0 or None compares system CPU times elapsed since last call or module import
     psutil.cpu_percent()
 
     while True:
-        net_in, net_out = get_net_usage(1)
+        net_in, net_out = get_net_usage(setts.update_interval)
         if net_in < 0.02 or net_out < 0.01:
             low_net_intervals += 1
         else:
             low_net_intervals = 0
         print(psutil.cpu_percent())
-        if psutil.cpu_percent() < cpu_load_threshold:
+        if psutil.cpu_percent() < setts.cpu_load_threshold:
             low_cpu_intervals += 1
         else:
             low_cpu_intervals = 0
 
-        if low_net_intervals > net_load_threshold and low_cpu_intervals > cpu_interval_threshold:
+        if low_net_intervals > setts.net_interval_threshold and low_cpu_intervals > setts.cpu_interval_threshold:
             # maybe make a popup prompts here
             os.system("shutdown /s /t 1")
 
         print(f"low_cpu_intervals {low_cpu_intervals} \n low_network {low_net_intervals}")
-        sleep(1)
 
 
 if __name__ == '__main__':
