948173831fa1c88369bf60715a5ca9d98f227473
MartinPek <martin.pek@web.de>
2023-08-30 02:08:57 +0200

    logAnalyzer changed out histogramm to plt

--- logAnalyzer/logAnalyzer.py
+++ logAnalyzer/logAnalyzer.py
@@ -15,6 +15,7 @@ def main():
     file_count = 0
     attempts = 0
     results = []
+    bins = []
     for file in files:
         print(file.name)
         f = open(file, "r")
@@ -25,20 +26,28 @@ def main():
                 attempts += 1
 
         file_count += 1
+        if attempts not in bins:
+            bins.append(attempts)
         print(attempts)
         results.append(attempts)
         f.close()
 
     med = statistics.median(results)
+    results, counts = np.unique(results, return_counts=True)
+    print(results)
+    print(counts)
+    print(file_count)
 
-
-    bin = 20
-    plt.hist(results, histtype='barstacked', bins=bin, rwidth=0.7, range=(0, 20))
-    plt.axvline(med, color='r')  # vertical
-    plt.gca().set(title='Pod airlock riddle', xlabel='Attempts', ylabel="number of groups");
+    fig, ax = plt.subplots(gridspec_kw={})
+    # plt.hist(results, histtype='barstacked', bins=bins, rwidth=0.7)
+    # plt.axvline(med, color='r')  # vertical
+    ax.bar(results, counts, width=0.7, linewidth=0.7)
+    ax.set(xlim=(0 ,21), xticks=range(0,21),
+           ylim=(0, 12))
+    plt.axvline(x=med, color='r', label="median")
+    plt.grid()
+    plt.gca().set(title=f'Pod airlock riddle from {file_count} games', xlabel='Attempts', ylabel="number of groups")
     plt.show()
-
-
 
 
 if __name__ == '__main__':
